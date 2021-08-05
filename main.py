4f20e8d8a65dbcfe28371d90fcd842e36ac7c35a
knhoman <knhoman@gmail.com>
2021-08-06 00:20:35 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.window_width, self.settings.window_height))
+        pygame.display.set_caption('AlienInvasion')
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+
+        while True:
+            """Отслеживание событий клавиатуры и мыши"""
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #При каждом проходе цикла перерисовывается экран
+            self.screen.fill(self.settings.bg_color)
+
+            """Отображение последнего прорисоанного экрана"""
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    """Создание экземпляра класса и запуск игры"""
+
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """Класс для хранения всех натсроек игры AlienInvasion"""
+
+
+    def __init__(self):
+        """Инициализирует настройки игры"""
+
+        #параметры экрана
+        self.window_width = 1280
+        self.window_height = 720
+        self.bg_color = (188, 188, 188)
